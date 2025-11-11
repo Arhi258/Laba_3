3f04d20a372fc3d39862a64da006aa57b85ec3a7
arhi258 <arhi258@yandex.ru>
2025-11-11 19:18:13 +0300

    Финально-финальные изменения

--- Tasks/Difficult_task.py
+++ Tasks/Difficult_task.py
@@ -9,17 +9,26 @@ if __name__ == '__main__':
     n = 0
     c = 0
 
+    sign = 1    # (-1) ** n
+    factorial_part = 1.0    # factorial(2 * n)
+    pi2_sq = (math.pi / 2) ** 2
+    pi_power = 1.0    # (pi / 2) ** (2 * n)
+    x_power = x    # x ** (4 * n + 1), при n = 0 -> x ** 1
+
     while True:
         # Вычисление члена ряда.
-        term = (
-            ((-1) ** n * (math.pi / 2) ** (2 * n)) /
-            (math.factorial(2 * n) * (4 * n + 1)) *
-            x ** (4 * n + 1)
-        )
+        term = sign * pi_power * x_power / (factorial_part * (4 * n + 1))
         c += term
+
         # Условие точности.
         if abs(term) < 1e-10:
             break
+
+        # Подготовка для следующего n
         n += 1
+        sign = -sign
+        factorial_part *= (2 * n - 1) * (2 * n)
+        pi_power *= pi2_sq
+        x_power *= x ** 4
 
     print(f"C({x}) ≈ {c:.10f}, число членов ряда = {n + 1}")
