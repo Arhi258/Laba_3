fc23ff1eee0fdf09487bc7f464ca994cc05c86e0
arhi258 <arhi258@yandex.ru>
2025-11-11 17:31:52 +0300

    Изменил 3 задание

--- Tasks/Task 3.py
+++ Tasks/Task 3.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def get_word_form(number, forms):
-    """
-    Возвращает правильную форму слова в зависимости от числа.
-    forms: кортеж из трех форм.
-    """
-    last_digit = number % 10
-    last_two = number % 100
-
-    if 11 <= last_two <= 14:
-        return forms[2]
-    elif last_digit == 1:
-        return forms[0]
-    elif last_digit in (2, 3, 4):
-        return forms[1]
-    else:
-        return forms[2]
-
 
 if __name__ == '__main__':
     time = int(input("Введите время, которое дается амебе на размножение (часы): "))
@@ -26,7 +9,4 @@ if __name__ == '__main__':
     number_of_divisions = time // 3
     number_of_amoebas = 2 ** number_of_divisions
 
-    hour_word = get_word_form(time, ('час', 'часа', 'часов'))
-    amoeba_word = get_word_form(number_of_amoebas, ('клетка', 'клетки', 'клеток'))
-
-    print(f"За {time} {hour_word} будет {number_of_amoebas} {amoeba_word}")
+    print(f"За {time} ч. количество клеток составит: {number_of_amoebas} ")
